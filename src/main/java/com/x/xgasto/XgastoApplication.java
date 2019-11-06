391695eb370fd4bb1954ddd344258fc5ba8cd955
Luiz Henrique <luizhenriqque15@gmail.com>
2019-11-06 17:33:33 -0400

    Started SpringBoot

---
+++ src/main/java/com/x/xgasto/XgastoApplication.java
@@ -0,0 +1,13 @@
+package com.x.xgasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgastoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgastoApplication.class, args);
+    }
+
+}
